5d5c5e320a4872299a826d40881df04cf0dbe734
McPuto <stefano.mazzuka@gmail.com>
2018-04-10 13:22:05 +0200

    Gen2 creado.

--- G08P2/src/base/Main.java
+++ G08P2/src/base/Main.java
@@ -4,6 +4,7 @@ import GUI.Menu;
 // https://people.cs.uct.ac.za/~jkenwood/JasonBrownbridge.pdf
 public class Main {
 	public static void main(String[] args) {		
+		Gen2 g = new Gen2();
 		Menu menu = new Menu();
 		menu.setVisible(true);
 	}
